b35d04ceb08429f9b529f320d659af14a68fb0a3
bnichs5 <bnichs51259@gmail.com>
2021-03-20 13:14:57 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	chmod +x on-complete.sh
 fi
 
-wget https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
-wget https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
+wget https://github.com/bnichs5/DL-Rclone.conf/raw/master/dht.dat
+wget https://github.com/bnichs5/DL-Rclone.conf/raw/master/dht6.dat
 tracker_list() {
-	curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat
+	curl -Ns https://raw.githubusercontent.com/bnichs5/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat
 }
 echo "bt-tracker=$tracker_list" >> aria2c.conf
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
